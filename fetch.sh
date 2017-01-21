8a041895ebffc6de7be4f0fdb19f40e632556479
Alf Eaton <eaton.alf@gmail.com>
2017-01-21 13:59:45 +0000

    Update files

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-wget --continue 'http://cufts2.lib.sfu.ca/knowledgebase/CUFTS_complete_20120801.tgz' --output-document=cufts.tar.gz
+wget 'http://cufts2.lib.sfu.ca/knowledgebase/CUFTS_complete_20170101.tgz' --output-document='cufts.tar.gz'
 
 mkdir cufts
 tar -xvzf cufts.tar.gz --directory cufts
 rm cufts/update.xml
 
-find cufts/ -type f -exec iconv -f iso-8859-1 -t utf-8 "{}" -o "{}-utf8" \;
+#find cufts/ -type f -exec iconv -f iso-8859-1 -t utf-8 "{}" -o "{}-utf8.tsv" \;
+
+find cufts/ -type f -exec mv "{}" "{}.tsv" \;
